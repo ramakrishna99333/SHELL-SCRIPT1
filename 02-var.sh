a7b1faabba5bfdd20368b8a7eaf9c9f8eb2e9030
ramkrishnagaddam <ram99441@gmail.com>
2024-12-11 10:04:13 +0530

    updated

--- 02-var.sh
+++ 02-var.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DATE =$(date)
+echo "executing the script on ${DATE}"
 PERSON1=$1
 PERSON2=$2
 echo "${PERSON1}: Hi ${PERSON2}"
 echo "${PERSON2}: Hi ${PERSON1}"
 
-echo "executing the script on ${DATE}"
+
